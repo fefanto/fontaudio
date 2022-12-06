1bd163d866d4fbd7a9112e66fa81e7d6c85b110c
Michelangelo Nottoli <m.nottoli@gmail.com>
2022-12-06 15:42:26 +0100

    choose folder for yarn svgclean (#13)
    Co-authored-by: Michelangelo Nottoli <michelangelo@neuraldsp.com>

--- scripts/svgcleaner.js
+++ scripts/svgcleaner.js
@@ -3,23 +3,26 @@ const path = require("path");
 const fs = require("fs");
 const { exec } = require("child_process");
 
-const directoryPath = "./svgs";
-fs.readdir(directoryPath, function(err, files) {
+var myArgs = process.argv.slice(2);
+console.log('svgcleaner.js - optimizing all svgs in ', myArgs[0]);
+const directoryPath = myArgs[0];
+
+fs.readdir(directoryPath, function (err, files) {
   if (err) {
     return console.log("Unable to scan directory: " + err);
   }
   var count = 0;
-  files.forEach(function(file) {
+  files.forEach(function (file) {
     if (file.split(".").pop() == "svg") {
       count++;
 
       console.log(`Optimizing ${file}...`);
       exec(
         // --disable=ConvertPathData,cleanupNumericValues
-        `./node_modules/svgo/bin/svgo --pretty --config \'./scripts/svgcleaner.yml\' -q ${path.join(
+        `./node_modules/svgo/bin/svgo --pretty --config \'./scripts/svgcleaner.yml\' -q \"${path.join(
           directoryPath,
           file
-        )}`,
+        )}\"`,
         (err, stdout, stderr) => {
           if (err) {
             console.log(`error while executing svgo on ${file} : ${err}`);
